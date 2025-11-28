d0e6c422e388ad0a3107bba362dc6ac7ea1f5d2f
ming-0120 <sarah500288@gmail.com>
2025-11-28 12:07:21 +0900

    DB 환경변수 세팅

--- config/settings.py
+++ config/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carebridge',          # 우리가 만든 DB 이름
+        'USER': 'carebridge_user',     # 방금 만든 전용 계정
+        'PASSWORD': 'cb1234',         # 네가 설정한 비밀번호
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+
 
 
 # Password validation
