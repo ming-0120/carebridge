40dd62ed69067b093f35ece5063c93011b6bb438
chldyddns-cmd <chldyddns707@gmail.com>
2025-12-01 17:29:15 +0900

    config/urls 파일 수정

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
-    path('hospitals/', include('apps.hospitals.urls')),  # ← 이게 중요!!
-
+    path('hospitals/', include('apps.hospitals.urls')),
+    path('mstaff/', include('app.emr.urls')),
 ]
