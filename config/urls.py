d83b28444dd8f5afa39e537b04812a1d60e1883f
ahw <dksldnjs22@gmail.com>
2025-12-01 17:25:53 +0900

    config/urls 수정

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls')),
     path('hospitals/', include('apps.hospitals.urls')),  # ← 이게 중요!!
+    path('emergency/', include('hospitals.urls')),
     path('admin-panel/', include('apps.admin_panel.urls')),
 
 ]
