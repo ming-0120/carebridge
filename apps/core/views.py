f57a813e1f1bc972b11971009990eb0932094d3d
ming-0120 <sarah500288@gmail.com>
2025-12-04 14:29:33 +0900

    #회원가입 부분 반영

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from django.utils import timezone
-from apps.db.models import DailyVisit
+from apps.db.models import DailyVisit, MedicalNewsletter
 from django.db.models import F
 
+
 def home(request):
-    # 오늘 날짜
     today = timezone.now().date()
-    
-    # 방문자 수 기록 (메인 페이지 접속 시)
+
     daily_visit, created = DailyVisit.objects.get_or_create(visit_date=today)
     daily_visit.visit_count = F('visit_count') + 1
     daily_visit.save(update_fields=['visit_count'])
-    
-    return render(request, 'core/home.html')
+
+    # 최신 5개 뉴스레터
+    newsletters = (
+        MedicalNewsletter.objects
+        .order_by('-created_at')[:3]   # created_at 컬럼 기준
+    )
+
+    return render(
+        request,
+        'core/home.html',
+        {
+            'newsletters': newsletters
+        }
+    )
+   
