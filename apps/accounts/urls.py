139ed1c148278abf2d50727fe162339316a81194
ming-0120 <sarah500288@gmail.com>
2025-12-11 12:35:23 +0900

    의료진 로그인 업로드

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,6 @@
 # apps/accounts/urls.py
 from django.urls import path
-from .views import login_view, register_view, logout_view, check_username, admin_login_view
+from .views import login_view, register_view, logout_view, check_username, admin_login_view, nurse_login_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('check-username/', check_username, name='check_username'),
 
     path('admin/login/', admin_login_view, name='admin_login'),
+    path('nurse/login/', nurse_login_view, name='nurse_login'),
 ]
