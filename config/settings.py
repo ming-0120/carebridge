08461ab728414a6d6408f945e48d2859dc63e68b
ming-0120 <sarah500288@gmail.com>
2025-12-12 09:02:08 +0900

    예약수정

--- config/settings.py
+++ config/settings.py
@@ -25,14 +25,7 @@ env = environ.Env(
     DEBUG=(bool, False),
 )
 env.read_env(os.path.join(BASE_DIR, '.env'))
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-coh6tb%m)as!^$=#@(aljpv_7fbih0)x0w-*(b7-mx(8iie*9u'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# 기본 설정들
 DEBUG = True
 SECRET_KEY = env('SECRET_KEY', default='django-insecure-coh6tb%m)as!^$=#@(aljpv_7fbih0)x0w-*(b7-mx(8iie*9u')
 
@@ -44,7 +37,12 @@ KAKAO_REDIRECT_URI = env(
 )
 KAKAO_MAP_JS_KEY = os.getenv("KAKAO_MAP_JS_KEY")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "52.79.116.240",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
